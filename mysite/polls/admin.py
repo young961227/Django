fa166b33b06de3477eaaf84d01ca811d84cbe69d
young sang lee <yohesa@nate.com>
2020-05-21 15:13:19 +0900

    part7

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -7,7 +7,7 @@ class Choiceinline(admin.TabularInline):
     extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question_text', 'pub_date', )
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
     inlines = [Choiceinline]
 

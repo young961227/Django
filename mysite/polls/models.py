efe51890e3162252c055db6f626c7bfab4f3ef7b
young sang lee <yohesa@nate.com>
2020-05-21 13:16:11 +0900

    part5

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,6 +2,9 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
